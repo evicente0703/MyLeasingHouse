6a3dfd70fb8aadd3dac5303a7bb317099d153585
Edwin Vicente <evicente0703@gmail.com>
2019-10-30 14:50:41 -0600

    Completo el SeedDB

--- MyLeasingHouse/MyLeasingHouse.Web/Startup.cs
+++ MyLeasingHouse/MyLeasingHouse.Web/Startup.cs
@@ -38,7 +38,7 @@ namespace MyLeasingHouse.Web
                 cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
-
+            services.AddTransient<SeedDb>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
