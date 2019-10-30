2032b1de7ab9a77c65132418a493666f566acd01
Edwin Vicente <evicente0703@gmail.com>
2019-10-30 00:28:26 -0600

    La clase Owners La BD DataText para la base de datos

---
+++ MyLeasingHouse/MyLeasingHouse.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MyLeasingHouse.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ MyLeasingHouse/MyLeasingHouse/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyLeasingHouse.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
