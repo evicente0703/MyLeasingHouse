2fb740166d6a5c3fca9600c878324286d3a68eac
Edwin Vicente <evicente0703@gmail.com>
2019-11-08 15:02:30 -0600

    Ingresa imagenes a la vista como a la BBDD

---
+++ MyLeasingHouse/MyLeasingHouse.Web/Helpers/ImageHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace MyLeasing.Web.Helpers
+{
+    public class ImageHelper : IImageHelper
+    {
+        public async Task<string> UploadImageAsync(IFormFile imageFile)
+        {
+            //un Guid es un metodo de numeros y letras 
+            var guid = Guid.NewGuid().ToString();
+            var file = $"{guid}.jpg";
+            var path = Path.Combine(
+                //esto es para que se aguarde dinamicamente ya que se puede remotamente o en la nube
+                Directory.GetCurrentDirectory(),
+                "wwwroot\\images\\Properties",
+                file);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(stream);
+            }
+            //~ esto es una berdulilla agara la ruta de la imagen
+            return $"~/images/Properties/{file}";
+        }
+    }
+}
+
--- MyLeasingHouse/MyLeasingHouse.Web/Startup.cs
+++ MyLeasingHouse/MyLeasingHouse.Web/Startup.cs
@@ -59,6 +59,7 @@ namespace MyLeasingHouse.Web
             services.AddScoped<IUserHelper, UserHelper>();
             services.AddScoped<ICombosHelper, CombosHelper>();
             services.AddScoped<IConverterHelper, ConverterHelper>();
+            services.AddScoped<IImageHelper, ImageHelper>();
             
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
