52f5da3af3771c3ecd0b1cafbe44ea6262b159f7
Edwin Vicente <evicente0703@gmail.com>
2019-11-12 12:24:50 -0600

    API Meda Los Datos en Posment

--- MyLeasingHouse/MyLeasingHouse.Web/Controllers/API/OwnersController.cs
+++ MyLeasingHouse/MyLeasingHouse.Web/Controllers/API/OwnersController.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using MyLeasing.Web.Data;
-using MyLeasingHouse.Common.Models;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyLeasing.Common.Models;
+using MyLeasing.Web.Data;
+using MyLeasing.Web.Data.Entities;
+using MyLeasingHouse.Common.Models;
 
-namespace MyLeasingHouse.Web.Controllers.API
+
+
+namespace MyLeasing.Web.Controllers.API
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -17,7 +22,6 @@ namespace MyLeasingHouse.Web.Controllers.API
             _dataContext = dataContext;
         }
 
-
         [HttpPost]
         [Route("GetOwnerByEmail")]
         public async Task<IActionResult> GetOwnerByEmailAsync(EmailRequest request)
@@ -28,12 +32,78 @@ namespace MyLeasingHouse.Web.Controllers.API
             }
 
             var owner = await _dataContext.Owners
+                .Include(o => o.User)
+                .Include(o => o.Properties)
+                .ThenInclude(p => p.PropertyType)
+                .Include(o => o.Properties)
+                .ThenInclude(p => p.PropertyImages)
+                .Include(o => o.Contracts)
+                .ThenInclude(c => c.Lessee)
+                .ThenInclude(l => l.User)
                 .FirstOrDefaultAsync(o => o.User.Email.ToLower() == request.Email.ToLower());
+
             if (owner == null)
             {
                 return NotFound();
             }
-            return Ok(owner);
+
+            var response = new OwnerResponse
+            {
+                Id = owner.Id,
+                FirstName = owner.User.FirstName,
+                LastName = owner.User.LastName,
+                Address = owner.User.Address,
+                Document = owner.User.Document,
+                Email = owner.User.Email,
+                PhoneNumber = owner.User.PhoneNumber,
+                //? esto hace validacion de null
+                Properties = owner.Properties?.Select(p => new PropertyResponse
+                {
+                    Address = p.Address,
+                    Contracts = p.Contracts?.Select(c => new ContractResponse
+                    {
+                        EndDate = c.EndDate,
+                        Id = c.Id,
+                        IsActive = c.IsActive,
+                        Lessee = ToLessesResponse(c.Lessee),
+                        Price = c.Price,
+                        Remarks = c.Remarks,
+                        StartDate = c.StartDate
+                    }).ToList(),
+                    HasParkingLot = p.HasParkingLot,
+                    Id = p.Id,
+                    IsAvailable = p.IsAvailable,
+                    Neighborhood = p.Neighborhood,
+                    Price = p.Price,
+                    PropertyImages = p.PropertyImages?.Select(pi => new PropertyImageResponse
+                    {
+                        Id = pi.Id,
+                        ImageUrl = pi.ImageFullPath
+                    }).ToList(),
+                    PropertyType = p.PropertyType.Name,
+                    Remarks = p.Remarks,
+                    Rooms = p.Rooms,
+                    SquareMeters = p.SquareMeters,
+                    Stratum = p.Stratum
+                }).ToList()
+            };
+
+            return Ok(response);
+        }
+
+        private LesseeResponse ToLessesResponse(Lessee lessee)
+        {
+            return new LesseeResponse
+            {
+                Id = lessee.Id,
+                Address = lessee.User.Address,
+                Document = lessee.User.Document,
+                Email = lessee.User.Email,
+                FirstName = lessee.User.FirstName,
+                LastName = lessee.User.LastName,
+                PhoneNumber = lessee.User.PhoneNumber
+            };
         }
     }
 }
+
