6e76658335686505e8c09d6977bd98b94855f71b
Edwin Vicente <evicente0703@gmail.com>
2019-11-14 14:02:07 -0600

    Funciona modo Avion y en Posman Corre los datos

---
+++ MyLeasingHouse/MyLeasingHouse.Common/Services/IApiService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using MyLeasing.Common.Models;
+
+namespace MyLeasing.Common.Services
+{
+    public interface IApiService
+    {
+        Task<Response<OwnerResponse>> GetOwnerByEmailAsync(
+            string urlBase,
+            string servicePrefix, 
+            string controller, 
+            string tokenType, 
+            string accessToken, 
+            string email);
+        Task<Response<TokenResponse>> GetTokenAsync(
+            string urlBase, 
+            string servicePrefix, 
+            string controller, 
+            TokenRequest request);
+        Task<bool> CheckConnectionAsync(string url);
+    }
+}
---
+++ MyLeasingHouse/MyLeasingHousePrism/MyLeasingHousePrism/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,109 @@
+﻿using MyLeasing.Common.Models;
+using MyLeasing.Common.Services;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyLeasingHousePrism.ViewModels
+{
+
+    public class LoginPageViewModel : ViewModelBase
+    {
+        private readonly IApiService _apiService;
+        private string _password;
+        private bool _isRunning;
+        private bool _isEnabled;
+        private DelegateCommand _loginCommand;
+
+        public LoginPageViewModel(INavigationService navigationService,
+            IApiService apiService) : base(navigationService)
+        {
+            _apiService = apiService;
+            Title = "Login";
+            IsEnabled = true;
+            
+        }
+        //repasar vien esto ya que es como un singlenton
+        // ?? significa que es igual a null
+        public DelegateCommand LoginCommand => _loginCommand ?? (_loginCommand = new DelegateCommand(Login));
+
+
+        public string Email { get; set; }
+
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
+        }
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set => SetProperty(ref _isEnabled, value);
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter an email.", "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter a password.", "Accept");
+                return;
+            }
+
+            IsRunning = true;
+            IsEnabled = false;
+
+            var url = App.Current.Resources["UrlAPI"].ToString();
+            var connection = await _apiService.CheckConnectionAsync(url);
+            if (!connection)
+            {
+                IsEnabled = true;
+                IsRunning = false;
+                await App.Current.MainPage.DisplayAlert("Error", "Check the internet connection.", "Accept");
+                return;
+            }
+
+
+
+
+            var request = new TokenRequest
+            {
+                Password = Password,
+                Username = Email
+            };
+
+            
+            var response = await _apiService.GetTokenAsync(url, "Account", "/CreateToken", request);
+
+
+            IsRunning = false;
+            IsEnabled = true;
+
+            if (!response.IsSuccess)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "User or password incorrect.", "Accept");
+                Password = string.Empty;
+                return;
+            }
+            var token = response.Result;
+
+            await App.Current.MainPage.DisplayAlert("Ok", "We are making progress!", "Accept");
+        }
+    }
+}
+
